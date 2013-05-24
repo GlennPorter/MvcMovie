fa99a5d6f33e6b000b8054274c6f45a68a0788c9
Glenn Porter <glenn.porter@live.com>
2013-05-24 13:38:55 +0100

    Initial commit.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers {
+    public class HelloWorldController : Controller {
+        public string Index() {
+            return "This is my <b>default</b> action...";
+        }
+
+        public string Welcome(string name, int numTimes = 1) {
+            return HttpUtility.HtmlEncode("Hello " + name + ", numTimes is: " + numTimes);
+        }
+    }
+}
